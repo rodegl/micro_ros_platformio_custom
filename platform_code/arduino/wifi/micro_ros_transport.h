1f9791e057a20df39cdcb28f638fa9cbf59a90bb
rodegl <roderico.garcia.leal@intel.com>
2024-06-24 13:00:06 -0600

    Error logs added for debugging

--- platform_code/arduino/wifi/micro_ros_transport.h
+++ platform_code/arduino/wifi/micro_ros_transport.h
@@ -1,16 +1,34 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-struct micro_ros_agent_locator {
+struct micro_ros_agent_locator
+{
     IPAddress address;
     int port;
 };
 
-static inline void set_microros_wifi_transports(char * ssid, char * pass, IPAddress agent_ip, uint16_t agent_port){
-	WiFi.begin(ssid, pass);
+static inline void set_microros_wifi_transports(char *ssid, char *pass, IPAddress agent_ip, uint16_t agent_port)
+{
+    WiFi.begin(ssid, pass);
+    int i = 0;
 
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        if (i == 0)
+        {
+            Serial.println("Connecting to WiFi...");
+        } else if (i > 0) {
+            Serial.print(".");
+        }
+        i ++;
+        
+    }
+
+    if (WiFi.status() == WL_CONNECTED)
+    {  
+        Serial.println("");
+        Serial.println("The connection to the WiFi mwetwork has been STABLISHED!");
     }
 
     static struct micro_ros_agent_locator locator;
@@ -19,10 +37,9 @@ static inline void set_microros_wifi_transports(char * ssid, char * pass, IPAddr
 
     rmw_uros_set_custom_transport(
         false,
-        (void *) &locator,
+        (void *)&locator,
         platformio_transport_open,
         platformio_transport_close,
         platformio_transport_write,
-        platformio_transport_read
-    );
+        platformio_transport_read);
 }
